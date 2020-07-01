7905ee0595291deb3d350f8537c2883482275e74
hulyak <57311842+hulya95@users.noreply.github.com>
2020-07-01 13:57:50 -0700

    feat : pass the state of the term and trigger termChange with onChange event

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,10 +2,30 @@ import React from 'react';
 import './SearchBar.css';
 
 class SearchBar extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      term : ''
+    }
+    this.search = this.search.bind(this);
+    this.handleTermChange = this.handleTermChange.bind(this);
+  }
+
+  // passes the state of the term to this.props.onSearch
+  search(){
+    this.props.onSearch(this.state.term);
+  }
+
+  // Sets the state of the search bar’s term to the event target’s value.
+  handleTermChange(e){
+    this.setState({ term : e.target.value});
+  }
   render(){
     return (
       <div className="SearchBar">
-      <input placeholder="Enter A Song, Album, or Artist" />
+      <input placeholder="Enter A Song, Album, or Artist" 
+          onChange={this.handleTermChange} onSearch={this.search}
+      />
       <button className="SearchButton">SEARCH</button>
     </div>
     )
