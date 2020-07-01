4e2f6811164b994d040c7d09669689fd0f5f8e74
hulyak <57311842+hulya95@users.noreply.github.com>
2020-07-01 00:05:47 -0700

    create a constructor and method to adding tracks with addTrack method

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,15 +2,22 @@ import React from 'react';
 import './Track.css';
 
 class Track extends React.Component{
-
+  constructor(props){
+    super(props);
+    this.addTrack = this.addTrack.bind(this);
+  }
   // add or remove songs
   renderAction(){
     if(this.props.isRemoval){
       return <button className='Track-action'> - </button>
     }else {
-      return <button className='Track-action'> + </button>
-    }
-    
+      return <button className='Track-action' onClick={this.addTrack}> + </button>
+    } 
+  }
+
+  // Use it to add this.props.track to the playlist.onAdd prop from App.js
+  addTrack(){
+    this.props.onAdd(this.props.track)
   }
   render(){
     return(
