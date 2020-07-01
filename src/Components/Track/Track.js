22fb5599a85e27128a184932c6008f4db31dd170
hulyak <57311842+hulya95@users.noreply.github.com>
2020-06-30 21:49:03 -0700

    render track component and add a dynamic button

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import './Track.css';
 
-export class Track extends React.Component{
-  // renderAction(){
-  //   if(isRemoval){
+class Track extends React.Component{
 
-  //   }
-  //   <button>-</button>
-  // }
+  // add or remove songs
+  renderAction(){
+    if(this.props.isRemoval){
+      return <button className='Track-action'> - </button>
+    }else {
+      return <button className='Track-action'> + </button>
+    }
+    
+  }
   render(){
     return(
       <div className="Track">
       <div className="Track-information">
-      {/* <h3><!-- track name will go here --></h3> */}
-      {/* <p><!-- track artist will go here--> | <!-- track album will go here --></p> */}
+      <h3>{this.props.track.name}</h3> 
+      <p> {this.props.track.artist}| {this.props.track.album} </p> 
       </div>
-      {/* <button className="Track-action"><!-- + or - will go here --></button> */}
+      {/* dynamic button based on isRemoval prop */}
+      {this.renderAction()} 
       </div>
     )
   }
 }
+export default Track;
